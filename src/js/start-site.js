46be7ccdd3ce82ff4e22fe20824d9170f80e3959
JuliaZaykaS <sys55@mail.ru>
2021-07-08 15:09:45 +0300

    провела рефакторинг у Георгия
    взяла ссылку из рефов на контейнер для фильмов и добавила свою функцию для рендера

--- src/js/start-site.js
+++ src/js/start-site.js
@@ -1,12 +1,15 @@
 import { page, getTrendItems } from './base-api.js'; /* -- 'page' на будущее для пагинации */
 import cardForm from '../hbs/cardForm.hbs';
+import { refs } from './refs.js';
+import { renderFilms } from './renderFilms.js';
 
-const listFilm = document.querySelector('.films-list');
+// const listFilm = document.querySelector('.films-list');
 
 async function getMarcup() {
   const data = await getTrendItems();
-  const result = cardForm(data.results);
+  // const result = cardForm(data.results);
+  renderFilms(data.results);
 
-  listFilm.insertAdjacentHTML('beforeend', result);
+  // listFilm.insertAdjacentHTML('beforeend', result);
 }
 getMarcup();
