6d7cf341897f2fa98de7f581ef02e1e6fc422150
JuliaZaykaS <sys55@mail.ru>
2021-07-09 12:05:36 +0300

    сделала кнопку и скролл вверх, остались стили

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import './js/home-btn.js';
 import './js/logoClick.js';
 
 import './js/start-site.js';
-
+import './js/scroll-to-top.js';
--- src/js/refs.js
+++ src/js/refs.js
@@ -5,6 +5,10 @@ export const refs = {
     libraryBtns: document.querySelector('.js-library-btns'),
     logo: document.querySelector('.js-logo'),
     spinner: document.querySelector('.js-spinner'),
+    btnToTop: document.getElementById('btn-top'),
+    sectionHeader: document.querySelector('.js-header'),
+
+
 
 
 }
