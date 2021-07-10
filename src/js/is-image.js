1dbfad815eb75424266ed3c71c4949dae5573c88
JuliaZaykaS <sys55@mail.ru>
2021-07-10 19:09:55 +0300

    осталось затянуть картинку с локалки

---
+++ src/js/is-image.js
@@ -0,0 +1,36 @@
+// import '../images/plug-image.png';
+// console.log('../images/plug-image.png');
+export function checkHasFilmImage(data) {
+    const filmImg = document.querySelectorAll('.js-film-image');
+    // console.log(filmImg);
+    // [...filmImg].forEach((elem) => {
+    //     console.log(elem.src);
+    //     if (elem.src !== '') return;
+    //     // elem.src = '../images/plug-image.png';
+
+    // })
+    // SORORITY GIRL'S REVENGE
+    // console.log(data);
+    // console.dir(File);
+    // console.log(File.webkitRelativePath);
+    // const objectURL = window.URL.createObjectURL();
+    // console.log(objectURL);
+    data.forEach((elem, index) => {
+        // console.log(images);
+        // console.log(elem);
+        // console.log(index);
+        // console.log(elem.poster_path);
+        if (elem.poster_path === null) {
+            // filmImg[index].src = 'images/plug-image.png';
+            console.log(filmImg[index]);
+            // filmImg[index].src = 'plug-image.png';
+            console.log(filmImg[index]);
+            // filmImg[index].src = 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Felis_silvestris_silvestris.jpg';
+        }
+
+
+    });
+
+
+
+}
--- src/js/renderFilms.js
+++ src/js/renderFilms.js
@@ -1,10 +1,14 @@
 import {refs } from './refs.js';
 import cardForm from '../hbs/cardForm.hbs';
 import {getGenres} from './genres.js';
-import {getReleaseYear} from './years.js';
+import { getReleaseYear } from './years.js';
+import { checkHasFilmImage } from './is-image.js';
+
 
 export function renderFilms(array) {
     refs.filmList.insertAdjacentHTML('beforeend', cardForm(array));
     getGenres(array);
     getReleaseYear(array);
+    checkHasFilmImage(array);
+
 }
