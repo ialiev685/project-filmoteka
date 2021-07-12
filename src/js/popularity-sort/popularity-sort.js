b0fa00179b5a6429c8f9c920c01c880cef63d5d1
Alexey Boklogov <kashira87@mail.ru>
2021-07-12 21:49:11 +0300

    сделал начальную загрузку из локалсторадж

--- src/js/popularity-sort/popularity-sort.js
+++ src/js/popularity-sort/popularity-sort.js
@@ -6,6 +6,11 @@ const sortDay = refs.dropdownListPopularSortDay;
 const sortWeek = refs.dropdownListPopularSortWeek;
 const sortValue = refs.dropdownPopularSortValue;
 
+export const PopularitySort = {
+  DAY: 'day',
+  WEEK: 'week',
+};
+
 sortValue.addEventListener('click', onDropdownListClick);
 
 
@@ -27,6 +32,7 @@ function onRemoveSortInputClick(e) {
 function onSortDayClick() {
     sortValue.textContent = 'за день';
     
+    forWriteStorageClick();
     serverRequestMoviesDay();
     removeSortInput();
 };
@@ -34,6 +40,7 @@ function onSortDayClick() {
 function onSortWeekClick() {
     sortValue.textContent = 'за неделю';
 
+    forWriteStorageClick();
     serverRequestMoviesWeek();
     removeSortInput();
 };
@@ -43,4 +50,24 @@ function removeSortInput() {
     sortDay.removeEventListener('click', onSortDayClick);
     sortWeek.removeEventListener('click', onSortWeekClick);
     window.removeEventListener('click', onRemoveSortInputClick);
+};
+
+//--------------запись в localStorage:
+
+if (localStorage.getItem('popularity') === null) {
+    localStorage.setItem('popularity', PopularitySort.DAY);
+}
+else if (localStorage.getItem('popularity') === PopularitySort.DAY) {
+    sortValue.textContent = 'за день';
+}
+else if (localStorage.getItem('popularity') === PopularitySort.WEEK) {
+    sortValue.textContent = 'за неделю';
+};
+
+export function forWriteStorageClick() {
+    if (sortValue.textContent === 'за день') {
+        localStorage.setItem('popularity', PopularitySort.DAY);
+    } else if (sortValue.textContent === 'за неделю') {
+        localStorage.setItem('popularity', PopularitySort.WEEK);
+    }
 };
