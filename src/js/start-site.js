f40dab9ecf52516639c127885b683864159129f4
Ilfat Aleiv <ialiev685@gmail.com>
2021-07-12 19:33:58 +0300

    доработка ночной темы в footer

--- src/js/start-site.js
+++ src/js/start-site.js
@@ -1,19 +1,12 @@
 import { getTrendItems } from './base-api.js'; /* -- 'page' на будущее для пагинации */
-// import cardForm from '../hbs/cardForm.hbs'; удали
+
 import { refs } from './refs.js';
 
-import { renderFilms } from "./renderFilms.js";
-// import render from "htmlparser2/node_modules/dom-serializer";
-
-// const listFilm = document.querySelector(".films-list");
+import { renderFilms } from './renderFilms.js';
 
 async function getMarcup(page) {
   const data = await getTrendItems(page);
   renderFilms(data.results);
-  // const result = cardForm(data.results);
-
-  // listFilm.insertAdjacentHTML("beforeend", result);
-
 }
 refs.spinner.classList.add('spinner-hidden');
 getMarcup(1);
