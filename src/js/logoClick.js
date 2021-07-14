b97308f9915a4ce7e271188d4d1a7fa79c922099
Denis Tsibaev <denis.tsibaev@gmail.com>
2021-07-14 21:31:57 +0700

    Два исправления
    logoClick.js закомменитровал как повторяющийся скрипт с  home-btn.js. Внёс поправку в margin для пагинации

--- src/js/logoClick.js
+++ src/js/logoClick.js
@@ -1,22 +1,22 @@
-import { refs } from './refs.js';
-import { getTrendItems } from './base-api.js';
-import { renderFilms } from './renderFilms.js';
+// import { refs } from './refs.js';
+// import { getTrendItems } from './base-api.js';
+// import { renderFilms } from './renderFilms.js';
 
-refs.logo.addEventListener('click', onLogoClick);
-
-
-async function onLogoClick(e) {
-    e.preventDefault();
-    refs.filmList.innerHTML = '';
-    if (!refs.libraryBtns.classList.contains('library-btns-hidden') || refs.searchContainer.classList.contains('header-input-hidden')) {
-        refs.libraryBtns.classList.add('library-btns-hidden');
-        refs.searchContainer.classList.remove('header-input-hidden');
-
-    };
-    refs.spinner.classList.remove('spinner-hidden');
-    const data = await getTrendItems(1);
-    renderFilms(data.results);
-    refs.spinner.classList.add('spinner-hidden');
+// refs.logo.addEventListener('click', onLogoClick);
 
 
-}
+// async function onLogoClick(e) {
+//     e.preventDefault();
+//     refs.filmList.innerHTML = '';
+//     if (!refs.libraryBtns.classList.contains('library-btns-hidden') || refs.searchContainer.classList.contains('header-input-hidden')) {
+//         refs.libraryBtns.classList.add('library-btns-hidden');
+//         refs.searchContainer.classList.remove('header-input-hidden');
+
+//     };
+//     refs.spinner.classList.remove('spinner-hidden');
+//     const data = await getTrendItems(1);
+//     renderFilms(data.results);
+//     refs.spinner.classList.add('spinner-hidden');
+
+
+// }
