4b67fef2d7e63c9a2f379281cb0d047e9eeadb1b
Ilfat Aleiv <ialiev685@gmail.com>
2021-07-14 22:52:06 +0300

    finish

--- src/js/hintSearch.js
+++ src/js/hintSearch.js
@@ -37,7 +37,8 @@ function renderHint(data) {
 
   refs.hintEl.innerHTML = markup;
   window.addEventListener('keyup', closeWithKey);
-  window.addEventListener('keydown', activeArrow);
+  window.addEventListener('keydown', activeArrowAndChoose);
+  window.addEventListener('mousemove', onHover);
   refs.hintEl.addEventListener('click', addValueInput);
 }
 
@@ -61,13 +62,13 @@ function correctionVote(data) {
 function removeHintBox() {
   refs.hintEl.innerHTML = '';
   refs.hintEl.classList.add('is-hidden');
-  window.removeEventListener('keyup', closeWithKey);
+  removeListener();
 }
 
 function closeWithKey(e) {
   if (e.code === 'Escape' || e.code === 'Enter') {
     removeHintBox();
-    window.removeEventListener('keyup', closeWithKey);
+    removeListener();
   }
 }
 
@@ -79,27 +80,75 @@ function addValueInput(e) {
   }
 }
 
-function activeArrow(e) {
-  const rowsHintList = document.querySelectorAll('.search-hint-table .row-table');
-  if (currentIndex === null) {
+function activeArrowAndChoose(e) {
+  const rowsHintList = getArrayHintList();
+
+  //начальная активация сверху / снизу
+  if (currentIndex === null && e.code === 'ArrowDown') {
+    clearCurrent();
     currentIndex = 0;
+    rowsHintList[currentIndex].classList.add('current');
+    scrollToCurrent();
+    return;
+  } else if (currentIndex === null && e.code === 'ArrowUp') {
+    clearCurrent();
+    currentIndex = 19;
+    rowsHintList[currentIndex].classList.add('current');
+    scrollToCurrent();
+    return;
   }
 
   const lenRows = rowsHintList.length;
-  console.log(lenRows);
 
-  if (e.code === 'ArrowDown' && currentIndex < lenRows) {
-    rowsHintList[currentIndex].classList.remove('current');
+  //передвижение вверх / вниз
+  if (e.code === 'ArrowDown' && currentIndex < lenRows - 1) {
+    clearCurrent();
     currentIndex += 1;
-
     rowsHintList[currentIndex].classList.add('current');
-  }
-  if (e.code === 'ArrowUp' && currentIndex > 0) {
-    rowsHintList[currentIndex].classList.remove('current');
+    scrollToCurrent();
+  } else if (e.code === 'ArrowUp' && currentIndex > 0) {
+    clearCurrent();
     currentIndex -= 1;
-
     rowsHintList[currentIndex].classList.add('current');
+    scrollToCurrent();
   }
 
-  console.log('текущий индекс', currentIndex);
+  //выбрать значение
+  if (e.code === 'Enter' && currentIndex !== null) {
+    const value = rowsHintList[currentIndex].children[0].textContent.trim();
+    refs.headerInput.value = value;
+  }
+}
+
+function onHover(e) {
+  if (e.target.parentElement.className === 'row-table') {
+    clearCurrent();
+    e.target.parentElement.classList.add('current');
+    getArrayHintList().forEach((el, index, array) => {
+      if (array[index].classList.contains('current')) {
+        currentIndex = index;
+      }
+    });
+  }
+}
+
+function clearCurrent() {
+  getArrayHintList().forEach((el, index, array) => {
+    array[index].classList.remove('current');
+  });
+}
+
+function scrollToCurrent() {
+  const activeEl = document.querySelector('.row-table.current');
+  activeEl.scrollIntoView({ block: 'end' });
+}
+
+function getArrayHintList() {
+  return document.querySelectorAll('.search-hint-table .row-table');
+}
+
+function removeListener() {
+  window.removeEventListener('keyup', closeWithKey);
+  window.removeEventListener('keydown', activeArrowAndChoose);
+  window.removeEventListener('mousemove', onHover);
 }
