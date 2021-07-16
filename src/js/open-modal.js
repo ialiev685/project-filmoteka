d8fce3c20a8a652548f691107f18cc4ebf7a722b
Ilfat Aleiv <ialiev685@gmail.com>
2021-07-16 18:16:21 +0300

    доработка завершена

--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -1,75 +1,73 @@
 import { refs } from './refs.js';
 import ButtonAction from './button-action.js';
+import movie from '../hbs/film-modal.hbs';
+import { checkHasFilmModalImage } from './is-image.js';
 
 const btnSwitch = new ButtonAction({
-    textAdd: "add to",
-    textRemove: "remove from"
+  textAdd: 'add to',
+  textRemove: 'remove from',
 });
 
 refs.filmList.addEventListener('click', onMovieClick);
 
 async function onMovieClick(e) {
-    
-    if (e.target.classList.value !== 'card-overlay') {
-        return
-    };
-    const movieId = e.target.dataset.value;
-    const article = await fetchFilm(movieId);
-    await appendArticlesMarkup(article);
-    const closeButton = document.querySelector('[data-action="close-modal"]');
-    const backdrop = document.querySelector('.backdrop');
-    
-    toggleClass(backdrop);
-    closeModal(closeButton, backdrop)
-};
-   
+  if (e.target.classList.value !== 'card-overlay') {
+    return;
+  }
+  const movieId = e.target.dataset.value;
+  const article = await fetchFilm(movieId);
+  appendArticlesMarkup(article);
+  checkHasFilmModalImage(article);
+  const closeButton = document.querySelector('[data-action="close-modal"]');
+  const backdrop = document.querySelector('.backdrop');
+
+  toggleClass(backdrop);
+  closeModal(closeButton, backdrop);
+}
 
 function fetchFilm(movieId) {
-    const KEY = '222d2b89e8701088edcf9049fa569980';
-    const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${KEY}&language=en-US`;
+  const KEY = '222d2b89e8701088edcf9049fa569980';
+  const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${KEY}&language=en-US`;
 
-    return fetch(url)
-      .then(response => response.json());
-};
+  return fetch(url).then(response => response.json());
+}
 
 function appendArticlesMarkup(article) {
-   const newFilmMarkup= btnSwitch.addButtonText(article);
-    refs.body.insertAdjacentHTML('afterbegin', movie(newFilmMarkup));
-    const buttonWatched = document.querySelector('.js-watched');
-    const buttonQueue = document.querySelector('.js-queue');
+  const newFilmMarkup = btnSwitch.addButtonText(article);
+  refs.body.insertAdjacentHTML('afterbegin', movie(newFilmMarkup));
+  const buttonWatched = document.querySelector('.js-watched');
+  const buttonQueue = document.querySelector('.js-queue');
 
-    btnSwitch.clickButtonModal(buttonWatched, buttonQueue, article.id, newFilmMarkup);;
-};
+  btnSwitch.clickButtonModal(buttonWatched, buttonQueue, article.id, newFilmMarkup);
+}
 
 function toggleClass(backdrop) {
-    
-    backdrop.classList.toggle('is-hidden') 
-};
+  backdrop.classList.toggle('is-hidden');
+}
 
 function closeModal(closeButton, backdrop) {
-    
-    closeButton.addEventListener('click', onButtonClick);
-    backdrop.addEventListener('click', onBackdropClick);
-    window.addEventListener('keydown', onEscKeyPress);
-    
-    function onButtonClick() {
-     
-        toggleClass(backdrop);
-        function removeMovie() {
-            backdrop.remove();
-        };
-        setTimeout(removeMovie, 500);
-        
-        window.removeEventListener('keydown', onEscKeyPress);
-        backdrop.removeEventListener('click', onBackdropClick);
-    }
-    function onEscKeyPress(evt) {
-        if (evt.code === 'Escape') {
-            onButtonClick()}
-    };
-    function onBackdropClick(evt) {
-        if (evt.currentTarget === evt.target) {
-            onButtonClick()}
-    };
-};
+  closeButton.addEventListener('click', onButtonClick);
+  backdrop.addEventListener('click', onBackdropClick);
+  window.addEventListener('keydown', onEscKeyPress);
 
+  function onButtonClick() {
+    toggleClass(backdrop);
+    function removeMovie() {
+      backdrop.remove();
+    }
+    setTimeout(removeMovie, 500);
+
+    window.removeEventListener('keydown', onEscKeyPress);
+    backdrop.removeEventListener('click', onBackdropClick);
+  }
+  function onEscKeyPress(evt) {
+    if (evt.code === 'Escape') {
+      onButtonClick();
+    }
+  }
+  function onBackdropClick(evt) {
+    if (evt.currentTarget === evt.target) {
+      onButtonClick();
+    }
+  }
+}
--- src/js/renderFilms.js
+++ src/js/renderFilms.js
@@ -5,32 +5,33 @@ import { getReleaseYear } from './years.js';
 import { checkHasFilmImage } from './is-image.js';
 import { getVote } from './vote-avarage.js';
 import ButtonAction from './button-action.js';
+import { renderPagination } from './pagination.js';
 
 const btnSwitch = new ButtonAction({
   textAdd: 'add to',
   textRemove: 'remove from',
 });
 
-export function renderFilms(array) {
-  const newArr = array.slice();
-  const newFilmsMarkup = newArr.results.map(elem => {
+export function renderFilms(array, valueSeatch = 'empty') {
+  refs.filmList.innerHTML = '';
+  const newFilmsMarkup = array.results.map(elem => {
     return btnSwitch.addButtonText(elem);
   });
   refs.filmList.insertAdjacentHTML('beforeend', cardForm(newFilmsMarkup));
-  btnSwitch.clickButtonOverlay(newFilmsMarkup);
-  getGenres(array);
-  getReleaseYear(array);
-  getVote(array);
-  checkHasFilmImage(array);
-}
-import { renderPagination } from './pagination.js';
-
-export function renderFilms(array, valueSeatch = 'empty') {
-  refs.filmList.innerHTML = '';
-  refs.filmList.insertAdjacentHTML('beforeend', cardForm(array.results));
   renderPagination(array.total_pages, array.page, valueSeatch); // передаем пагинацию
+  btnSwitch.clickButtonOverlay(newFilmsMarkup);
   getGenres(array.results);
   getReleaseYear(array.results);
   getVote(array.results);
   checkHasFilmImage(array.results);
 }
+
+// export function renderFilms(array, valueSeatch = 'empty') {
+//   refs.filmList.innerHTML = '';
+//   refs.filmList.insertAdjacentHTML('beforeend', cardForm(array.results));
+//   renderPagination(array.total_pages, array.page, valueSeatch); // передаем пагинацию
+//   getGenres(array.results);
+//   getReleaseYear(array.results);
+//   getVote(array.results);
+//   checkHasFilmImage(array.results);
+// }
